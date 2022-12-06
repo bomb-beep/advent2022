7c5a2e73fb9ab7cf1a82948402a6fe137dcec539
bo <bo.bratsberg@funkweb.org>
2022-12-06 10:58:23 +0100

    day 5

--- 05/crane_puzzle.py
+++ 05/crane_puzzle.py
@@ -1,29 +1,35 @@
 import regex as re
 
+
 text = open("input.txt").read()
 
 starting_pos,move_list = re.split("\n\n",text)
 
 print(starting_pos)
 print("blank space here separator!")
-#print(move_list)
 cargo = {
 
 }
 indexes = {
 
 }
-#print(list(re.finditer("\s*\[([A-Z])\]\s*",starting_pos)))
+
+def move_crates(source,dest,num):
+	while num > 0:
+		cargo[dest].append(cargo[source].pop())
+		num -= 1
+
+def move_crates_bulk(source,dest,num):
+	index = len(cargo[source])-num
+	crane = cargo[source][index:]
+	cargo[source] = cargo[source][:index]
+	cargo[dest].extend(crane)
+
 starting_pos = starting_pos.split("\n")
-#print(list(re.finditer("\s*(\d+)\s*",starting_pos))[0].group(1))
-#print(list(range(len(starting_pos)-1,0)))
 for line in reversed(starting_pos):
-	print(list(re.finditer("\s*(\d+)\s*",line)))
-	print(list(re.finditer("\s*\[(\[A-Z])\]\s*",line)))
 	for match in re.finditer("\s*(\d+)\s*",line):
 		if match:
-			#print(match.group(1)+"|")
-			cargo[int(match.group(1))] = []#[match.start(1)]
+			cargo[int(match.group(1))] = []
 			indexes[match.start(1)] = int(match.group(1))
 	for match in re.finditer("\s*\[([A-Z])\]\s*",line):
 		if match:
@@ -31,3 +37,32 @@ for line in reversed(starting_pos):
 
 print(cargo)
 print(indexes)
+total_crates = 0
+for stack in cargo.values():
+	total_crates += len(stack)
+print(total_crates)
+
+
+for line in move_list.split("\n"):
+	match = re.search("move (?P<number>\d+) from (?P<source>\d) to (?P<dest>\d)",line)
+	if match:
+		try:
+			move_crates_bulk(int(match.group("source")),int(match.group("dest")),int(match.group("number")))
+		except IndexError:
+			print(match.string)
+			print(cargo)
+
+
+print(cargo)
+total_crates = 0
+for stack in cargo.values():
+	total_crates += len(stack)
+print(total_crates)
+
+final_arrangement = ""
+for stack in range(1,10):
+	if cargo[stack]:
+		final_arrangement += cargo[stack][-1]
+	else:
+		final_arrangement += " "
+print(final_arrangement)
