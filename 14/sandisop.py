8dd6d8dff3892d2057761dab6ec51c54ab94e5c3
bo <bo.bratsberg@funkweb.org>
2022-12-20 11:13:13 +0100

    at last, day 14

--- 14/sandisop.py
+++ 14/sandisop.py
@@ -5,7 +5,7 @@ xmin,xmax = x_offset,x_offset
 ymax = 0
 grid = np.array([[3]])
 print(grid.shape)
-output = open("output","w")
+output = open("output.txt","w")
 
 def draw_wall(wall):
 	global grid
@@ -24,8 +24,6 @@ def draw_wall(wall):
 
 def expand_grid(xmin,xmax,ymax):
 	global grid,x_offset
-	#xmin,xmax,ymax = int(xmin),int(xmax),int(ymax)
-	output.write("expand {} {} {} {}\n".format(grid.shape,x_offset,xmin,xmax))
 
 	if xmin < x_offset:
 		insert = np.zeros((x_offset - xmin,grid.shape[1]))
@@ -39,7 +37,6 @@ def expand_grid(xmin,xmax,ymax):
 		grid = np.append(grid,insert,axis = 1)
 
 	grid[:,-1] = 1
-	output.write("expanded {} {} {} {}\n".format(grid.shape,x_offset,xmin,xmax))
 
 	
 
@@ -49,45 +46,33 @@ def drop_sand():
 	sand = np.where(grid == 3)
 	pot_pos = [[0,1],[-1,1],[1,1]]
 	settled = False
-	#print(type(sand[0]))
 	while not settled:
 		settled = True
 		if not grid[sand]:
 			grid[sand] = 4
 		try:
 			for element in pot_pos:
-				#print(sand,element)
 				new_pos = (int(sand[0]+element[0]),int(sand[1]+element[1]))
-				print(sand,new_pos, grid[new_pos])
-				output.write(f"{new_pos[0]+x_offset},{new_pos[1]}, {x_offset}, {xmax}\n")
-				if new_pos[0] + x_offset in range(x_offset,xmax+1):
-					#print(new_pos)
+				if new_pos[0] + x_offset in range(x_offset,xmax):
 					if grid[new_pos] in [0,4]:
 						sand = new_pos
 						settled = False
 						break
-					#return False
-					#print(new_pos,type(new_pos),type(new_pos[0]))
 				else:
 					old_offset = x_offset
 					expand_grid(min(x_offset,new_pos[0]+x_offset),max(xmax,new_pos[0]+x_offset+1),ymax)
-					#xmin,xmax = min(xmin,new_pos[0]+x_offset),max(xmax,new_pos[0]+x_offset+1)
 					sand = (sand[0] + old_offset - x_offset,sand[1])
 					new_pos = (int(sand[0]+element[0]),int(sand[1]+element[1]))
 					if grid[new_pos] in [0,4]:
 						sand = new_pos
 						settled = False
 						break
-					#print("expanded",new_pos[0]+x_offset,xmin,xmax)
 		except IndexError:
 			print("Index Error",grid.shape,sand,new_pos)
-			output.write(f"Index Error{grid.shape}, {sand}, {new_pos}\n")
-			#print(np.where(grid == 1))
+			output.write(f"Index Error{grid.shape}, {sand}, {new_pos[0] + x_offset}, {xmax}\n")
 
 			print_grid()
-		#print(sand)
 		if settled:
-			output.write(f"settled {sand[0]+x_offset},{sand[1]}\n")
 			if grid[sand] not in [1,2,3]:
 				grid[sand] = 2
 			else:
@@ -95,9 +80,7 @@ def drop_sand():
 			return sand
 		elif sand[1] > ymax:
 			return False
-		# if sand[1] == 1:
-		# 	print(sand[0] + x_offset,sand[1])
-		
+
 			
 		
 
@@ -120,39 +103,33 @@ def print_grid():
 		print(line)
 
 
-rocks = open("test_input.txt").read().split("\n")
-#rocks = ["498,4 -> 498,6 -> 496,6"]
+rocks = open("input.txt").read().split("\n")
 
 walls = []
 
 for line in rocks:
-	#print(np.where(grid==3))
 	wall = []
 	for coord in line.split(" -> "):
-		#print(coord)
 		x,y = coord.split(",")
 		wall.append((int(x),int(y)))
 		xmin = min(wall[-1][0],xmin)
 		xmax = max(wall[-1][0],xmax)
 		ymax = max(wall[-1][1],ymax)
 	walls.append(wall)
-	#draw_wall(wall)
-	#print_grid()
+
 
 xmax += 1
 ymax += 2
-print(xmin,xmax,ymax)
 expand_grid(xmin,xmax,ymax)
-print(np.where(grid == 1))
 for wall in walls:
 	draw_wall(wall)
-#print(x_offset + np.where(grid==3)[0],np.where(grid==3)[1])
 
-print(x_offset,grid.shape)
-#print(grid)
+counter = 0
 while drop_sand():
-	print_grid()
-print(x_offset,grid.shape)
+	counter += 1
+	if counter % 100 == 0:
+		print(counter)
+	
 
 print_grid()
 print(len(grid[grid == 2]))
